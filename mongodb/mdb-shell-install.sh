5df85d3ca48d64ff909b3ed7ca50598e02023e74
guimarczewski <guimarczewski@gmail.com>
2023-01-07 20:03:51 +0000

    mdb-shell-install executavel

--- mongodb/mdb-shell-install.sh
+++ mongodb/mdb-shell-install.sh
@@ -1,9 +1,26 @@
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+up () {
+    wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
-sudo apt-get update
+    sudo apt-get update
 
-sudo apt-get install -y mongodb-mongosh
+    sudo apt-get install -y mongodb-mongosh
 
-mongosh $MDB_CONNECTION_STRING;
+    mongosh $MDB_CONNECTION_STRING;
+}
+
+case $1 in
+  up)
+    up
+    ;;
+  config)
+    config
+    ;;
+  down)
+    down
+    ;;
+  *)
+    echo "Usage: $0 {up|config|down}"
+    ;;
+esac
