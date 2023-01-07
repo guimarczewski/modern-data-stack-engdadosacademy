217dc58de8ecf1768117f95b92bd3d2821885861
guimarczewski <guimarczewski@gmail.com>
2023-01-07 01:57:53 +0000

    demographics table update

--- mysql/demographics table.sql
+++ mysql/demographics table.sql
@@ -24,9 +24,29 @@ CREATE TABLE DEMOGRAPHICS(
     population_age_80_and_older INT
 );
 
-LOAD DATA LOCAL INFILE 'https://github.com/guimarczewski/modern-data-stack-engdadosacademy/blob/main/data/demographics.csv'
+-- mover o arquivo csv para dentro do container, para identificar o container_id basta usar o comando "docker ps", nesse caso 73c03dd063b4
+docker cp /workspace/modern-data-stack-engdadosacademy/data/demographics.csv 73c03dd063b4:/usr/local/
+
+LOAD DATA LOCAL INFILE '/usr/local/demographics.csv'
 INTO TABLE DEMOGRAPHICS
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+SELECT location_key, population, population_male, population_female FROM DEMOGRAPHICS LIMIT 10;
+
++--------------+------------+-----------------+-------------------+
+| location_key | population | population_male | population_female |
++--------------+------------+-----------------+-------------------+
+| AD           |      77265 |           58625 |             55581 |
+| AE           |    9890400 |         6836349 |           3054051 |
+| AF           |   38928341 |        19976265 |          18952076 |
+| AF_BAL       |    1509183 |               0 |                 0 |
+| AF_BAM       |     495557 |               0 |                 0 |
+| AF_BDG       |     549583 |               0 |                 0 |
+| AF_BDS       |    1054087 |               0 |                 0 |
+| AF_BGL       |    1014634 |               0 |                 0 |
+| AF_DAY       |     516504 |               0 |                 0 |
+| AF_FRA       |     563026 |               0 |                 0 |
++--------------+------------+-----------------+-------------------+
